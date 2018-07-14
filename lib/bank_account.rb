3b608c994921de7d5e3d96fd295a950ca981d7f4
Matthew <mdwareing@gmail.com>
2018-07-14 10:38:15 +0100

    Add default and configurable balances

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,6 +1,9 @@
 class BankAccount
 
-    def balance
-        0
+    attr_reader :balance
+    DEFAULT_BALANCE = 0
+
+    def initialize(account_setup = {})
+        @balance = account_setup.fetch(:starting_balance, DEFAULT_BALANCE)
     end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -2,7 +2,19 @@ require 'bank_account'
 
 describe BankAccount do
     it 'prints an integer balance' do
-        bank_account = BankAccount.new
+        bank_account = BankAccount.new()
         expect(bank_account.balance).to be_a_kind_of(Numeric)
+    end
+
+    it 'has a default bank balance of 0' do
+        DEFAULT_BALANCE = 0
+        bank_account = BankAccount.new()
+        expect(bank_account.balance).to eq DEFAULT_BALANCE
+    end
+
+    it 'has a configurable default balance' do
+        STARTING_BALANCE = 20
+        bank_account = BankAccount.new(starting_balance: STARTING_BALANCE)
+        expect(bank_account.balance).to eq STARTING_BALANCE
     end
 end
