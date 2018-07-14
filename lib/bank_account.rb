0c2cbf6041ca07700a7c2787ec71f0f835937726
Matthew <mdwareing@gmail.com>
2018-07-14 11:04:25 +0100

    Add account deposit and prevent negative deposit

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,8 +2,13 @@ class BankAccount
 
     attr_reader :balance
     DEFAULT_BALANCE = 0
-
+    MIN_ACCOUNT_BALANCE = 0
     def initialize(account_setup = {})
         @balance = account_setup.fetch(:starting_balance, DEFAULT_BALANCE)
+    end
+
+    def deposit(amount)
+        raise ArgumentError.new("You cannot deposit a negative amount") if amount < MIN_ACCOUNT_BALANCE
+        @balance += amount
     end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -2,13 +2,13 @@ require 'bank_account'
 
 describe BankAccount do
     it 'prints an integer balance' do
-        bank_account = BankAccount.new()
+        bank_account = BankAccount.new
         expect(bank_account.balance).to be_a_kind_of(Numeric)
     end
 
     it 'has a default bank balance of 0' do
         DEFAULT_BALANCE = 0
-        bank_account = BankAccount.new()
+        bank_account = BankAccount.new
         expect(bank_account.balance).to eq DEFAULT_BALANCE
     end
 
@@ -16,5 +16,15 @@ describe BankAccount do
         STARTING_BALANCE = 20
         bank_account = BankAccount.new(starting_balance: STARTING_BALANCE)
         expect(bank_account.balance).to eq STARTING_BALANCE
+    end
+
+    it 'has the ability to deposit funds' do
+        bank_account = BankAccount.new
+        expect{bank_account.deposit(20)}.to change{bank_account.balance}.by(20)
+    end
+
+    it 'cannot deposit negative amounts' do
+        bank_account = BankAccount.new
+        expect{bank_account.deposit(-50)}.to raise_exception("You cannot deposit a negative amount")
     end
 end
